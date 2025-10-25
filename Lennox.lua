6033ced54e3d0cdb0cb5faafd675c907083b3ba1
lennox2015 <lennox.pfeiffer2015@gmail.com>
2025-10-25 10:53:03 +0200

    Replace example code with print statement

--- Lennox.lua
+++ Lennox.lua
@@ -1,7 +1,1 @@
--- plain Lua example (desktop Lua interpreter)
-local chunk = loadstring("return 2 + 2")
-print(chunk())  -- prints 4
-
-local code = "for i=1,3 do print('hi', i) end"
-local f = loadstring(code)
-f()  -- executes the code in `code`
+print("bruhhhhhhhhhhhhhhhhhhhhhhh")
